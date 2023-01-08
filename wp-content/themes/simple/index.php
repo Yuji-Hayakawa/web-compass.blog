07b558b517dfa4fd57b44ed0e5594c407ae29824
haya <haya@MacBook-Air.local>
2023-01-08 11:58:20 +0900

    [simple]公式：head要素マークアップ

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -1,2 +1,24 @@
-<?php
-// Silence is golden.
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ページの紹介文">
+    <meta property="og:url" content="https://web-compass.blog">
+    <meta property="og:title" content="ページのタイトル">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="ページのサムネ画像">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="og:site_name" content="webコンパス">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@ここに自分のTwitterユーザ名">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="canonical" href="https://web-compass.blog">
+    <title>webコンパス</title>
+  </head>
+  <body>
+
+  </body>
+</html>
