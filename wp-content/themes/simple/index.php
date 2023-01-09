0168e251279417d3e4fdcfc78d557c5c890964d9
haya <haya@MacBook-Air.local>
2023-01-09 13:14:09 +0900

    [simple]公式：header要素マークアップ

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -2,9 +2,11 @@
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
+    <title>webコンパス</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="ページの紹介文">
+    <meta name="format-detection" content="telephone=no, email=no, address=no">
     <meta property="og:url" content="https://web-compass.blog">
     <meta property="og:title" content="ページのタイトル">
     <meta property="og:description" content="">
@@ -16,9 +18,31 @@
     <meta name="twitter:site" content="@ここに自分のTwitterユーザ名">
     <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
     <link rel="canonical" href="https://web-compass.blog">
-    <title>webコンパス</title>
   </head>
-  <body>
-
+  <body class="Page">
+    <div class="PageContainer" id="page-container">
+      <header class="PageHeader" id="page-header" role="banner">
+        <div class="inner">
+          <h1 class="HeaderPageId">
+            <a href="https://web-compass.blog">webコンパス</a>
+          </h1>
+          <p>国内シェアNo1サービスの「中の人」がブログ運営に役立つ情報をわかりやすくお届け！</p>
+          <nav class="HeaderGlobalNav" id="global-nav" role="navigation" aria-label="メインメニュー">
+            <div class="HeaderGlobalNav__contents">
+              <ul class="HeaderGlobalNav__list">
+                <li><a href="/category/wordpress/"><i class="fa-brands fa-wordpress"></i> WordPress</a></li>
+                <li><a href="/category/html/"><i class="fa-solid fa-code"></i>HTML</a></li>
+                <li><a href="/category/css/"><i class="fa-solid fa-brackets-curly"></i>CSS</a></li>
+                <li><a href="/category/javascript/"><i class="fa-brands fa-square-js"></i>JavaScript</a></li>
+                <li><a href="/category/work/"><i class="fa-solid fa-desktop"></i>Work</a></li>
+                <li><a href="/category/books/"><i class="fa-light fa-book-open"></i>Books</a></li>
+              </ul>
+            </div>
+          <!-- /.HeaderGlobalNav -->
+          </nav>
+        </div>
+      <!-- /.PageHeader -->
+      </header>
+    </div>
   </body>
 </html>
