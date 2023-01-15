2bc2043caf5fe6c40a51fb55727e7767611bd2fe
haya <haya@MacBook-Air.local>
2023-01-16 08:22:12 +0900

    [simple]公式：マークアップ（新着記事のWPループ化）

--- wp-content/themes/simple/functions.php
+++ wp-content/themes/simple/functions.php
@@ -1,3 +1,6 @@
 <?php
 // <title>タグを出力する
 add_theme_support( 'title-tag' );
+
+// アイキャッチ画像を使用可能にする
+add_theme_support( 'post-thumbnails' );
--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -36,18 +36,22 @@
             </section>
             <section class="_ArticlePart _New" id="new">
               <h2 class="_ArticlePart__title">新着記事</h2>
-              <article class="_ArticleItem">
-                <a href="/">
-                  <figure class="_ArticleItem__thumbnail">
-                    <img src="" alt="" width="200" height="166" decoding="async">
-                  </figure>
-                  <div class="_ArticleItem__meta">
-                    <h3 class="_ArticleItem__title">【実質無料】クリプト運用（DeFi）の「生放送セミナー」を実施します</h3>
-                    <p class="_ArticleItem__description">クリプト運用（DeFi）の生放送セミナーを実施します。僕は「約２億円」を仮想通貨の「DeFi」で運用しており、利回りは「6.7%」くらいです。セミナーでは「DeFiのメリット＆デメリット、リスク回避の方法、実際の使い方」を解説します。</p>
-                  </div>
-                </a>
-              <!-- /._ArticleItem TODO:このパーツを以下に9回繰り返す -->
-              </article>
+              <?php if ( have_posts() ): ?>
+                <?php while ( have_posts() ): the_post(); ?>
+                  <article <?php post_class('_ArticleItem'); ?> >
+                    <a href="<?php the_permalink(); ?>">
+                      <figure class="_ArticleItem__thumbnail">
+                        <?php the_post_thumbnail('medium') ?>
+                      </figure>
+                      <div class="_ArticleItem__meta">
+                        <h3 class="_ArticleItem__title"><?php the_title(); ?></h3>
+                        <?php the_excerpt(); ?>
+                      </div>
+                    </a>
+                  <!-- /._ArticleItem -->
+                  </article>
+                <?php endwhile; ?>
+              <?php endif; ?>
             <!-- /._ArticlePart -->
             </section>
             <nav class="Pagination" aria-label="ページネーション">
