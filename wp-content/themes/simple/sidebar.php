3203a7cf5f86de357301bd64d2ac8cc19e4ef119
haya <haya@MacBook-Air.local>
2023-01-16 09:04:10 +0900

    [simple]公式：マークアップ（sidebarのテンプレ化）

---
+++ wp-content/themes/simple/sidebar.php
@@ -0,0 +1,55 @@
+<aside class="Sidebar" id="sidebar">
+  <div class="Search" id="search">
+    <form action="https://web-compass.blog/" class="SearchForm" method="get" role="search">
+      <input type="text" name="s" value="" placeholder="検索" class="SearchForm__input">
+      <button type="submit" class="SearchForm__submit" aria-label="検索する"></button>
+    </form>
+  <!-- /.Search -->
+  </div>
+  <div class="_Menu" id="menu">
+    <ul class="_Menu__list">
+      <li><a href="/beginner/"><i class="fa-regular fa-star-half-stroke"></i>初心者向け</a></li>
+      <li><a href="/intermediate/"><i class="fa-solid fa-graduation-cap"></i>中級者向け</a></li>
+      <li><a href="/html/"><i class="fa-solid fa-code"></i>HTML</a></li>
+      <li><a href="/css/"><i class="fa-solid fa-palette"></i>CSS</a></li>
+      <li><a href="/wordpress/"><i class="fa-brands fa-wordpress"></i>WordPress</a></li>
+      <li><a href="/books/"><i class="fa-solid fa-book-open"></i>おすすめ本</a></li>
+    </ul>
+  <!-- /._Menu -->
+  </div>
+  <section class="Profile" id="profile">
+    <h4 class="Profile__author">はや氏</h4>
+    <figure class="Profile__authorImg">
+      <img src="" alt="はや氏の似顔絵" width="120" height="120" decoding="async">
+    </figure>
+    <p class="Profile__job">Webエンジニア</p>
+    <p class="Profile__text">WordPressが得意なWeb屋。HPcode代表。300件以上のWordPress案件を対応してきました。SE → 農家 → アフィリエイター → Web屋。生まれは三重県。<br>
+    ホームページ制作を承っております。お気軽にお問い合わせください！</p>
+    <a href="/profile/" rel="author">プロフィール詳細</a>
+  <!-- /.Profile -->
+  </section>
+  <section class="Category" id="category">
+    <h4 class="Category__title">Category</h4>
+    <ul class="Category__list">
+      <li><a href="/category/wordpress/">WordPress</a></li>
+      <li><a href="/category/html/">HTML</a></li>
+      <li><a href="/category/css/">CSS</a></li>
+      <li><a href="/category/javascript/">JavaScript</a></li>
+      <li><a href="/category/work/">Work</a></li>
+      <li><a href="/category/books/">Books</a></li>
+      <!-- TODO: 以下liタグ繰り返し -->
+    </ul>
+  <!-- /.Category -->
+  </section>
+  <section class="Archive" id="archive">
+    <h4 class="Archive__title">Archive</h4>
+    <ul class="Archive__list">
+      <li><a href="/2023/01/">2023年1月</a></li>
+      <li><a href="/2022/12/">2022年12月</a></li>
+      <li><a href="/2022/11/">2022年11月</a></li>
+      <!-- TODO: 以下liタグ繰り返し -->
+    </ul>
+  <!-- /.Archive -->
+  </section>
+<!-- /.Sidebar -->
+</aside>
