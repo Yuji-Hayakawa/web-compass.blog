8e119755534f984231b25df0464dcb9dd5faf48b
haya <haya@MacBook-Air.local>
2023-01-20 07:41:27 +0900

    [simple]公式：マークアップ（headテンプレ化）

---
+++ wp-content/themes/simple/head.php
@@ -0,0 +1,21 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="ページの紹介文">
+  <meta name="format-detection" content="telephone=no, email=no, address=no">
+  <meta property="og:url" content="https://web-compass.blog/">
+  <meta property="og:title" content="ページのタイトル">
+  <meta property="og:description" content="">
+  <meta property="og:image" content="ページのサムネ画像">
+  <meta property="og:type" content="website">
+  <meta property="og:locale" content="ja_JP">
+  <meta property="og:site_name" content="webコンパス">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@ここに自分のTwitterユーザ名">
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="canonical" href="https://web-compass.blog">
+  <?php
+  wp_enqueue_style('font-awesome','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css');
+  wp_head();
+  ?>
+</head>
