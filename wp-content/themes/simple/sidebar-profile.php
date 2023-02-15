a95c774a1e86e565cf91bab018dcbbc67dd6cfbd
haya <haya@MacBook-Air.local>
2023-02-15 09:11:43 +0900

    [simple]公式：スタイリング（Profile実装）

--- wp-content/themes/simple/sidebar-profile.php
+++ wp-content/themes/simple/sidebar-profile.php
@@ -1,11 +1,15 @@
 <section class="Profile" id="profile">
-  <h4 class="Profile__author">はや氏</h4>
-  <figure class="Profile__authorImg">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/portrait.jpg" alt="" width="120" height="120" decoding="async">
-  </figure>
-  <p class="Profile__job">Webエンジニア</p>
+  <div class="Profile__img">
+    <h4 class="Profile__author">はや氏</h4>
+    <div class="Profile__authorImg">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/portrait.jpg" alt="" width="120" height="120" decoding="async">
+    </div>
+  </div>
+  <p class="Profile__job">Webコーダー</p>
   <p class="Profile__text">WordPressが得意なWeb屋。HPcode代表。300件以上のWordPress案件を対応してきました。SE → 農家 → アフィリエイター → Web屋。生まれは三重県。<br>
   ホームページ制作を承っております。お気軽にお問い合わせください！</p>
-  <a href="/profile/" rel="author">プロフィール詳細</a>
+  <div class="Profile__detail">
+    <a href="/profile/" rel="author">プロフィール詳細 ></a>
+  </div>
 <!-- /.Profile -->
 </section>
