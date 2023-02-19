5e002e740f33ac45ade5c130e7f52e58807cb53d
haya <haya@MacBook-Air.local>
2023-02-20 08:23:32 +0900

    [simple]公式：スタイリング（shareBtn）

--- wp-content/themes/simple/single.php
+++ wp-content/themes/simple/single.php
@@ -24,9 +24,9 @@
               <footer class="ArticleContent__footer">
                 <div class="_ShareBtn">
                   <ul class="_ShareBtn__list">
-                    <li class="_ShareBtn__item -twitter"><a href="" target="_blank"><i class="fa-brands fa-twitter"></i>Tweet</a></li>
-                    <li class="_ShareBtn__item -feedly"><a href="" target="_blank"><i class="fa-solid fa-rss"></i>Feedly</a></li>
-                    <li class="_ShareBtn__item -copy"><a href=""><i class="fa-regular fa-copy"></i>Copy URL</a></li>
+                    <li class="_ShareBtn__item c-button -line"><a href=""><i class="fa-brands fa-line"></i>LINE</a></li>
+                    <li class="_ShareBtn__item c-button -twitter"><a href="" target="_blank"><i class="fa-brands fa-twitter"></i>Tweet</a></li>
+                    <li class="_ShareBtn__item c-button -feedly"><a href="" target="_blank"><i class="fa-solid fa-rss"></i>Feedly</a></li>
                   </ul>
                 <!-- /._ShareBtn -->
                 </div>
