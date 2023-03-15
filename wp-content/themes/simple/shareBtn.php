868e365b593394cfb126682266c0765f9f8c09da
haya <haya@MacBook-Air.local>
2023-03-16 08:40:18 +0900

    Scripted auto-commit on change (2023-03-16 08:40:18) by gitwatch.sh

--- wp-content/themes/simple/shareBtn.php
+++ wp-content/themes/simple/shareBtn.php
@@ -1,7 +1,7 @@
 <div class="_ShareBtn">
   <ul class="_ShareBtn__list">
     <li class="_ShareBtn__item c-button -line"><a href="https://social-plugins.line.me/lineit/share?url=<?php the_permalink(); ?>" target="_blank"><i class="fa-brands fa-line"></i> Tweet</a></li>
-    <li class="_ShareBtn__item c-button -twitter"><a href="https://social-plugins.line.me/lineit/share?url=<?php the_permalink(); ?>" target="_blank"><i class="fa-brands fa-twitter"></i> Tweet</a></li>
+    <li class="_ShareBtn__item c-button -twitter"><a href="" target="_blank"><i class="fa-brands fa-twitter"></i> Tweet</a></li>
     <li class="_ShareBtn__item c-button -feedly"><a href="" target="_blank"><i class="fa-solid fa-rss"></i> Feedly</a></li>
   </ul>
 <!-- /._ShareBtn -->
