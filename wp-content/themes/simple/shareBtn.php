15d67affce941dabd75f5263a79be2eb06b2031e
haya <haya@MacBook-Air.local>
2023-03-16 08:24:26 +0900

    Scripted auto-commit on change (2023-03-16 08:24:26) by gitwatch.sh

--- wp-content/themes/simple/shareBtn.php
+++ wp-content/themes/simple/shareBtn.php
@@ -1,8 +1,8 @@
 <div class="_ShareBtn">
   <ul class="_ShareBtn__list">
     <li class="_ShareBtn__item c-button -line">
-    <div class="line-it-button" data-lang="ja" data-type="share-b" data-env="REAL" data-url="https://web-compass.blog/" data-color="default" data-size="small" data-count="false" data-ver="3" style="display: none;"></div>
-<script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+      <div class="line-it-button" data-lang="ja" data-type="share-b" data-env="REAL" data-url="https://web-compass.blog/" data-color="default" data-size="small" data-count="false" data-ver="3" style="display: none;"></div>
+  <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
       <i class="fa-brands fa-line"></i> LINE</li>
     <li class="_ShareBtn__item c-button -twitter"><a href="" target="_blank"><i class="fa-brands fa-twitter"></i> Tweet</a></li>
     <li class="_ShareBtn__item c-button -feedly"><a href="" target="_blank"><i class="fa-solid fa-rss"></i> Feedly</a></li>
