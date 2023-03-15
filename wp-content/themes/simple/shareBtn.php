bd72476b18e4c789839d75b4a79de805dd6c0c6e
haya <haya@MacBook-Air.local>
2023-03-16 08:54:04 +0900

    Scripted auto-commit on change (2023-03-16 08:54:04) by gitwatch.sh

--- wp-content/themes/simple/shareBtn.php
+++ wp-content/themes/simple/shareBtn.php
@@ -7,6 +7,4 @@
 <!-- /._ShareBtn -->
 </div>
 
-<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-via="@HayaUji" data-lang="ja" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-
 <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-via="HayaUji" data-lang="ja" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
