436618ecbaa0ff05a044341f075c296f52eb180d
haya <haya@MacBook-Air.local>
2023-03-16 08:03:45 +0900

    Scripted auto-commit on change (2023-03-16 08:03:45) by gitwatch.sh

--- wp-content/themes/simple/single.php
+++ wp-content/themes/simple/single.php
@@ -22,14 +22,7 @@
               <!-- /.ArticleContentBody -->
               </div>
               <footer class="ArticleContentFooter">
-                <div class="_ShareBtn">
-                  <ul class="_ShareBtn__list">
-                    <li class="_ShareBtn__item c-button -line"><a href=""><i class="fa-brands fa-line"></i> LINE</a></li>
-                    <li class="_ShareBtn__item c-button -twitter"><a href="" target="_blank"><i class="fa-brands fa-twitter"></i> Tweet</a></li>
-                    <li class="_ShareBtn__item c-button -feedly"><a href="" target="_blank"><i class="fa-solid fa-rss"></i> Feedly</a></li>
-                  </ul>
-                <!-- /._ShareBtn -->
-                </div>
+                
                 <section class="Profile -author">
                   <h3 class="c-lv3HeadingSingle">この記事を書いた人</h3>
                   <div class="ProfileBox">
