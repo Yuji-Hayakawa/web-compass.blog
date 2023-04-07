6632681d3de7c558d703895896e878480c7c48bc
haya <haya@MacBook-Air.local>
2023-04-07 10:18:20 +0900

    [simple]公式：OGPなど

--- wp-content/themes/simple/head.php
+++ wp-content/themes/simple/head.php
@@ -9,23 +9,8 @@
   </script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php if ( is_404() ) : ?>
-    <meta name="robots" content="noindex, nofollow">
-  <?php else : ?>
-    <meta name="description" content="ページの紹介文">
-  <?php endif; ?>
   <meta name="format-detection" content="telephone=no, email=no, address=no">
-  <meta property="og:url" content="https://web-compass.blog/">
-  <meta property="og:title" content="ページのタイトル">
-  <meta property="og:description" content="">
-  <meta property="og:image" content="ページのサムネ画像">
-  <meta property="og:type" content="website">
-  <meta property="og:locale" content="ja_JP">
-  <meta property="og:site_name" content="webコンパス">
-  <meta name="twitter:card" content="summary">
-  <meta name="twitter:site" content="@ここに自分のTwitterユーザ名">
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-  <link rel="canonical" href="https://web-compass.blog">
+  <link rel="apple-touch-icon" sizes="180x180" href="https://web-compass.blog/wp-content/uploads/2023/04/1.png">
   <link rel="icon" href="/wp-content/themes/simple/favicon.ico">
   <?php wp_head(); ?>
 </head>
